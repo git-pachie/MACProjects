839c308b1c76e6eb8a4c43d11e049702ea6f1827
git-pachie <pachie@pachies-Mac.local>
2014-07-14 17:41:55 +0800

    ss

---
+++ MyNotificationSample/MyNotificationSampleTests/MyNotificationSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  MyNotificationSampleTests.m
+//  MyNotificationSampleTests
+//
+//  Created by pachie on 11/7/14.
+//  Copyright (c) 2014 Private. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyNotificationSampleTests : XCTestCase
+
+@end
+
+@implementation MyNotificationSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
