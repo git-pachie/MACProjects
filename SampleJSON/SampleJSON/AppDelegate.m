6220e6d3828dd12815c59ecd04e7e96e9b2d65d8
xcoderman <PachieMac@Archies-MacBook-Pro.local>
2014-08-18 02:10:25 +0800

    update

--- SampleJSON/SampleJSON/AppDelegate.m
+++ SampleJSON/SampleJSON/AppDelegate.m
@@ -67,6 +67,11 @@
     self.DevinceToken = @"1111111111";
 }
 
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"%@",userInfo);
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
